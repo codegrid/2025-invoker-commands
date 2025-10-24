da910ef0caef9ec505b83bea2097026397224ca5
Masataka Yakura <yakura@pxgrid.com>
2025-10-25 05:47:32 +0900

    classListを使う

--- invoker-support-message.js
+++ invoker-support-message.js
@@ -1,6 +1,6 @@
 if (!('command' in HTMLButtonElement.prototype)) {
   const warning = document.createElement('p');
-  warning.className = 'support-warning';
+  warning.classList.add('support-warning');
   warning.textContent = 'ブラウザーがInvoker Commandsに対応していません。';
   document.body.firstElementChild.after(warning);
 }
